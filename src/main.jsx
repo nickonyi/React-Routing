f00b6e3f2556bf72e36fbc41f334414feff8879e
phatommobb <nickoluoch1130@gmail.com>
2025-03-10 17:34:21 +0300

    client routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App.jsx';
-import Profile from './components/Profile.jsx';
-import ErrorPage from './components/ErrorPage.jsx';
+import routes from './components/routes.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: 'profile/:name',
-    element: <Profile />,
-  },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
