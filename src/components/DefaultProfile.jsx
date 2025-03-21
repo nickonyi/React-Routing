20d260eb614ad64329ad77e1b34f058e9bc8b04f
phatommobb <nickoluoch1130@gmail.com>
2025-03-21 14:01:03 +0300

    code

--- src/components/DefaultProfile.jsx
+++
@@ -1,3 +0,0 @@
-export default function DefaultProfile() {
-  return <p>Oooh nothing to see here!</p>;
-}
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import routes from './components/routes.jsx';
 
 const router = createBrowserRouter(routes);
 
